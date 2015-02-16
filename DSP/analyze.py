5390f57a1e5e241f488b39603015c4532a6d4332
Luis Munoz <luis.munoz@4dsp.com>
2015-02-16 17:19:35 -0600

    added support for binary offset and window functions

--- DSP/analyze.py
+++ DSP/analyze.py
@@ -4,29 +4,51 @@ from scipy.signal import triang
 from scipy.fftpack import fft
 import pylab as plt
 
+# data format, binary offset
+binary_offset = True # False = two's complement 
 # bit resolution
-r = 16 
-# sample rate
-fs = 368.64 *10**6
-fs = 368.64 
+r = 8
+# sample rate in MHz
+fs = 1250
 
 # load samples
-x = np.genfromtxt("./adc0.txt", dtype=None);
-# normalize the signal
-x  = x / 2.0**(r-1)
-
+x = np.genfromtxt("./data/adc0_fmc126.txt", dtype=None);
 N = x.size
+
+if binary_offset == True:
+	for num in range(N):
+		if x[num] < 0:
+			x[num] = x[num] + 2**r
+		else:
+			x[num] = abs(x[num])
+	# normalize the signal
+	x  = x / 2.0**(r)
+else:
+	# subtract one because MSB is sign bit and already represented with sign
+	x  = x / 2.0**(r-1)
+		
+# generate window function
+#window = np.blackman(N)
+#window = np.hamming(N)
+window = np.hanning(N)
+#window = np.bartlett(N)
+#window = np.ones(N)
+
+# roll the waveform to get correct phase
 #fftbuffer = np.roll(fftbuffer, -(fftbuffer.size/2))
 
-X = fft(x)
+# perform the FFT
+X = fft(x*window)
 mx = 20*np.log10(2.0  * 1/ N * abs(X))
 pX = np.angle(X)
 
+# plot time domain
 plt.subplot(2,1,1)
-plt.plot(x[:256])
+plt.plot(np.arange(256), x[:256])
 plt.title('Waveform Analysis')
 plt.ylabel('Time Domain')
 
+# plot frequency domain
 plt.subplot(2,1,2)
 xaxis= np.arange(N/2-1) * fs/N
 plt.plot(xaxis, mx[:N/2-1])
