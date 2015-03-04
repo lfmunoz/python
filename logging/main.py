c0bfab85d6ea55aaa9b53ead26383bbea50fd42c
Luis Munoz <luis.munoz@4dsp.com>
2015-03-04 16:49:27 -0600

    added formatting

--- logging/main.py
+++ logging/main.py
@@ -1,33 +1,48 @@
 import logging
 import logging.config
-#import test
+import test
 
 LOGGING_DICT = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s - %(levelname)-8s - %(module)s] %(message)s'
+        },
+        'simple': {
+            'format': '[%(asctime)s - %(levelname)-8s - %(module)s] %(message)s'
+        },
+    },
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': './debug.log',
+            'formatter': 'verbose',
         },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
             'stream': 'ext://sys.stdout',
+            'formatter': 'simple',
         },
     },
     'loggers': {
-        'test0': {
+        'main': {
             'handlers': ['console'],
             'level': 'DEBUG',
+            'propagate': True,
+        },
+        'test': {
+            'handlers': ['file'],
+            'level': 'INFO',
             'propagate': True,
         },
     },
 }
 
 logging.config.dictConfig(LOGGING_DICT)
-logger = logging.getLogger('test0')
+logger = logging.getLogger('main')
 
 logger.debug('debug message')
 logger.info('info message')
@@ -35,3 +50,5 @@ logger.warn('warn message')
 logger.error('error message')
 logger.critical('critical message')
 
+
+test.test()
--- logging/test.py
+++ logging/test.py
@@ -3,11 +3,9 @@ import logging
 logger0 = logging.getLogger(__name__)
 
 
-print "1. TEST.PY"
-
 def test():
     print "2. TEST.PY"
-    logger0.debug('debug message')
+    logger0.debug('1 debug message')
     logger0.info('info message')
     logger0.warn('warn message')
     logger0.error('error message')
